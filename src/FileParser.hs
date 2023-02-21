1fcdcc9df710755d3df910f54882896f3ebf6356
Lukáš Plevač <lukas@plevac.eu>
2023-02-21 11:43:21 +0100

    Added working file parser

--- src/FileParser.hs
+++ src/FileParser.hs
@@ -6,6 +6,15 @@
 --
 module FileParser where
 
+--
+-- Data structures
+--
+data Config = Config {
+    curve     :: Curve,
+    key       :: Key,
+    signature :: Signature
+} deriving (Show)
+
 data Point = Point { 
   x :: Integer,
   y :: Integer
@@ -20,16 +29,94 @@ data Curve = Curve {
     h :: Integer
 } deriving (Show)
 
-testCurve = Curve {
-    p = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F,
-    a = 0,
-    b = 7,
-    
-    g = Point {
-        x = 0x79BE667EF9DCBBAC55A06295CE870B07029BFCDB2DCE28D959F2815B16F81798,
-        y = 0x483ADA7726A3C4655DA4FBFC0E1108A8FD17B448A68554199C47D08FFB10D4B8
+data Key = Key {
+    d :: Integer,
+    q :: Integer
+} deriving (Show)
+
+data Signature = Signature {
+    r :: Integer,
+    s :: Integer
+} deriving (Show)
+
+
+defaultConfig = Config {
+    curve = Curve {
+        p = 0,
+        a = 0,
+        b = 0,
+        
+        g = Point {
+            x = 0,
+            y = 0
+        },
+
+        n = 0,
+        h = 0
     },
 
-    n = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141,
-    h = 1
+    key = Key {
+        d = 0,
+        q = 0
+    },
+
+    signature = Signature {
+        r = 0,
+        s = 0
+    }
 }
+
+--
+-- Data struct manager
+--
+
+updateCurveP (Curve _ a b (Point x y) n h) new     = Curve new a   b   (Point x y) n   h
+updateCurveA (Curve p _ b (Point x y) n h) new     = Curve p   new b   (Point x y) n   h
+updateCurveB (Curve p a _ (Point x y) n h) new     = Curve p   a   new (Point x y) n   h
+updateCurveN (Curve p a b (Point x y) _ h) new     = Curve p   a   b   (Point x y) new h
+updateCurveH (Curve p a b (Point x y) n _) new     = Curve p   a   b   (Point x y) n   new
+
+updateCurvePointX (Curve p a b (Point x y) n h) new = Curve p a b (Point new y  ) n h
+updateCurvePointY (Curve p a b (Point x y) n h) new = Curve p a b (Point x   new) n h
+
+updateKeyD (Key _ q) new = Key new q
+updateKeyQ (Key d _) new = Key d   new
+
+updateSignatureR (Signature _ s) new = Signature new s
+updateSignatureS (Signature r _) new = Signature r   new
+
+--
+-- File parser
+--
+parseFile :: String -> Config
+parseFile str = parseWords (words str) defaultConfig
+
+parseWords :: [String] -> Config -> Config
+parseWords ("Curve":"{":args)     cfg = parseCurve     args cfg
+parseWords ("Key":"{":args)       cfg = parseKey       args cfg
+parseWords ("Signature":"{":args) cfg = parseSignature args cfg
+parseWords []                     cfg = cfg
+
+parseCurve :: [String] -> Config -> Config
+parseCurve ("p:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveP (curve cfg) (read val::Integer) })
+parseCurve ("a:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveA (curve cfg) (read val::Integer) })
+parseCurve ("b:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveB (curve cfg) (read val::Integer) })
+parseCurve ("n:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveN (curve cfg) (read val::Integer) })
+parseCurve ("h:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveH (curve cfg) (read val::Integer) })
+parseCurve ("g:":"Point":"{":args) cfg  = parseCurvePoint args cfg
+parseCurve ("}":args)              cfg  = parseWords args cfg
+
+parseCurvePoint :: [String] -> Config -> Config
+parseCurvePoint ("x:":val:args) cfg  = parseCurvePoint args (cfg { curve = updateCurvePointX (curve cfg) (read val::Integer) })
+parseCurvePoint ("y:":val:args) cfg  = parseCurvePoint args (cfg { curve = updateCurvePointY (curve cfg) (read val::Integer) })
+parseCurvePoint ("}":args)      cfg  = parseCurve args cfg
+
+parseKey :: [String] -> Config -> Config
+parseKey ("d:":val:args) cfg  = parseKey args (cfg { key = updateKeyD (key cfg) (read val::Integer) })
+parseKey ("Q:":val:args) cfg  = parseKey args (cfg { key = updateKeyQ (key cfg) (read val::Integer) })
+parseKey ("}":args)      cfg  = parseWords args cfg
+
+parseSignature :: [String] -> Config -> Config
+parseSignature ("r:":val:args) cfg  = parseSignature args (cfg { signature = updateSignatureR (signature cfg) (read val::Integer) })
+parseSignature ("s:":val:args) cfg  = parseSignature args (cfg { signature = updateSignatureS (signature cfg) (read val::Integer) })
+parseSignature ("}":args)      cfg  = parseWords args cfg
--- src/Main.hs
+++ src/Main.hs
@@ -7,6 +7,6 @@ main = do
   args        <- getArgs
   let options = parseOptions args defaultOptions
   config  <- if optInputFile options == "" then getContents else readFile (optInputFile options)
+  let parsedConfig = parseFile config
   putStrLn (show options)
-  putStrLn (show testCurve)
-  putStrLn (config)
+  putStrLn (show parsedConfig)
