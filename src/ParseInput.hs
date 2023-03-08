636e71d60b02a6065bf3caa5a85b519aa8e3f294
Lukas Plevac <lukas@plevac.eu>
2023-03-08 14:58:25 +0000

    FIxed wall build

--- src/ParseInput.hs
+++ src/ParseInput.hs
@@ -5,7 +5,7 @@
 -- @author Lukáš Plevač <at> BUT FIT 
 --
 module ParseInput where
-import Types 
+import Types
 
 ---
 --- CLI Arguments parser part
@@ -20,26 +20,7 @@ parseOptions ("-s":args) opts  = parseOptions args (opts { optSing   = True   })
 parseOptions ("-v":args) opts  = parseOptions args (opts { optVerify = True   })
 
 parseOptions (arg:[])    opts  = parseOptions []   (opts { optInputFile = arg  })
-parseOptions (arg:args)  opts  = parseOptions args opts
-
----
---- File parser part
----
-
-updateCurveP (Curve _ a b (Point x y) n h) new     = Curve new a   b   (Point x y) n   h
-updateCurveA (Curve p _ b (Point x y) n h) new     = Curve p   new b   (Point x y) n   h
-updateCurveB (Curve p a _ (Point x y) n h) new     = Curve p   a   new (Point x y) n   h
-updateCurveN (Curve p a b (Point x y) _ h) new     = Curve p   a   b   (Point x y) new h
-updateCurveH (Curve p a b (Point x y) n _) new     = Curve p   a   b   (Point x y) n   new
-
-updateCurvePointX (Curve p a b (Point x y) n h) new = Curve p a b (Point new y  ) n h
-updateCurvePointY (Curve p a b (Point x y) n h) new = Curve p a b (Point x   new) n h
-
-updateKeyD (Key _ q) new = Key new q
-updateKeyQ (Key d _) new = Key d   new
-
-updateSignatureR (Signature _ s) new = Signature new s
-updateSignatureS (Signature r _) new = Signature r   new
+parseOptions (_:args)    opts  = parseOptions args opts --skip unknew argument
 
 --
 -- File parser
@@ -52,6 +33,7 @@ parseWords ("Curve":"{":args)     cfg = parseCurve     args cfg
 parseWords ("Key":"{":args)       cfg = parseKey       args cfg
 parseWords ("Signature":"{":args) cfg = parseSignature args cfg
 parseWords []                     cfg = cfg
+parseWords _                      cfg = (cfg { ok = False })
 
 parseCurve :: [String] -> Config -> Config
 parseCurve ("p:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveP (curve cfg) (read val::Integer) })
@@ -61,18 +43,22 @@ parseCurve ("n:":val:args)         cfg  = parseCurve args (cfg { curve = updateC
 parseCurve ("h:":val:args)         cfg  = parseCurve args (cfg { curve = updateCurveH (curve cfg) (read val::Integer) })
 parseCurve ("g:":"Point":"{":args) cfg  = parseCurvePoint args cfg
 parseCurve ("}":args)              cfg  = parseWords args cfg
+parseCurve _                       cfg = (cfg { ok = False })
 
 parseCurvePoint :: [String] -> Config -> Config
 parseCurvePoint ("x:":val:args) cfg  = parseCurvePoint args (cfg { curve = updateCurvePointX (curve cfg) (read val::Integer) })
 parseCurvePoint ("y:":val:args) cfg  = parseCurvePoint args (cfg { curve = updateCurvePointY (curve cfg) (read val::Integer) })
 parseCurvePoint ("}":args)      cfg  = parseCurve args cfg
+parseCurvePoint _               cfg = (cfg { ok = False })
 
 parseKey :: [String] -> Config -> Config
 parseKey ("d:":val:args) cfg  = parseKey args (cfg { key = updateKeyD (key cfg) (read val::Integer) })
 parseKey ("Q:":val:args) cfg  = parseKey args (cfg { key = updateKeyQ (key cfg) (read val::Integer) })
 parseKey ("}":args)      cfg  = parseWords args cfg
+parseKey _               cfg = (cfg { ok = False })
 
 parseSignature :: [String] -> Config -> Config
 parseSignature ("r:":val:args) cfg  = parseSignature args (cfg { signature = updateSignatureR (signature cfg) (read val::Integer) })
 parseSignature ("s:":val:args) cfg  = parseSignature args (cfg { signature = updateSignatureS (signature cfg) (read val::Integer) })
 parseSignature ("}":args)      cfg  = parseWords args cfg
+parseSignature _               cfg = (cfg { ok = False })
--- src/Types.hs
+++ src/Types.hs
@@ -27,7 +27,8 @@ defaultOptions = Options {
 data Config = Config {
     curve     :: Curve,
     key       :: Key,
-    signature :: Signature
+    signature :: Signature,
+    ok        :: Bool
 } deriving (Show)
 
 data Point = Point { 
@@ -55,6 +56,7 @@ data Signature = Signature {
 } deriving (Show)
 
 
+defaultConfig :: Config
 defaultConfig = Config {
     curve = Curve {
         p = 0,
@@ -78,5 +80,40 @@ defaultConfig = Config {
     signature = Signature {
         r = 0,
         s = 0
-    }
+    },
+
+    ok = True
 }
+
+updateCurveP :: Curve -> Integer -> Curve
+updateCurveP (Curve _ a b (Point x y) n h) new     = Curve new a   b   (Point x y) n   h
+
+updateCurveA :: Curve -> Integer -> Curve
+updateCurveA (Curve p _ b (Point x y) n h) new     = Curve p   new b   (Point x y) n   h
+
+updateCurveB :: Curve -> Integer -> Curve
+updateCurveB (Curve p a _ (Point x y) n h) new     = Curve p   a   new (Point x y) n   h
+
+updateCurveN :: Curve -> Integer -> Curve
+updateCurveN (Curve p a b (Point x y) _ h) new     = Curve p   a   b   (Point x y) new h
+
+updateCurveH :: Curve -> Integer -> Curve
+updateCurveH (Curve p a b (Point x y) n _) new     = Curve p   a   b   (Point x y) n   new
+
+updateCurvePointX :: Curve -> Integer -> Curve
+updateCurvePointX (Curve p a b (Point _ y) n h) new = Curve p a b (Point new y  ) n h
+
+updateCurvePointY :: Curve -> Integer -> Curve
+updateCurvePointY (Curve p a b (Point x _) n h) new = Curve p a b (Point x   new) n h
+
+updateKeyD :: Key -> Integer -> Key
+updateKeyD (Key _ q) new = Key new q
+
+updateKeyQ :: Key -> Integer -> Key
+updateKeyQ (Key d _) new = Key d   new
+
+updateSignatureR :: Signature -> Integer -> Signature
+updateSignatureR (Signature _ s) new = Signature new s
+
+updateSignatureS :: Signature -> Integer -> Signature
+updateSignatureS (Signature r _) new = Signature r   new
